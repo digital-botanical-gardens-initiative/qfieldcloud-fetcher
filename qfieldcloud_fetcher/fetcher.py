d9907e833e04203dde5ab7dac4dba7c0a43125ac
edouardbruelhart <edouard.brulhart@gmail.com>
2025-03-11 10:09:16 +0100

    Remove annoying prints

--- qfieldcloud_fetcher/fetcher.py
+++ qfieldcloud_fetcher/fetcher.py
@@ -142,8 +142,6 @@ for prefix, urls_jpg_by_layer in urls_jpg_by_project.items():
             file_name = url.split("/")[-1]
             dir_name = url.split("/")[-2]
 
-            print(f"prefix: {prefix}, dir_name: {dir_name}")
-            print(path_jpg)
             # Create the directory path for the downloaded file
             save_dir = path_jpg[prefix][dir_name]
 
