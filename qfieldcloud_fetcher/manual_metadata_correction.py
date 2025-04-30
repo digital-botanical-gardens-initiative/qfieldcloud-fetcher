d651b81207b88c5ce520484d531ec1af2322c8d8
edouardbruelhart <edouard.brulhart@gmail.com>
2025-04-30 14:01:41 +0200

    Remove annoying prints

--- qfieldcloud_fetcher/manual_metadata_correction.py
+++ qfieldcloud_fetcher/manual_metadata_correction.py
@@ -88,7 +88,6 @@ for root, _dirs, files in os.walk(inat_jpg_path):
                         break
 
             if not found:
-                print(f"found: {found}")
                 print(f"No data found for {unique_id}")
                 continue
 
